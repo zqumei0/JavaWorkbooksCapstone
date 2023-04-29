71d9da68d4639f70e9767f2d11f3387eeebaecfb
zqumei <zaid.qumei+os@gmail.com>
2023-04-29 17:46:10 -0400

    Task 3: Added Constructor to Trade Account Class

--- src/pojo/TradeAccount.java
+++ src/pojo/TradeAccount.java
@@ -3,6 +3,10 @@ package pojo;
 public abstract class TradeAccount {
   private String id;
 
+  public TradeAccount(String id) {
+    this.id = id;
+  }
+
   public String getId() {
     return this.id;
   }

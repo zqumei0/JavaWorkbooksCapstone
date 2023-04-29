f5ceafc6c1ff41d9b95b44290e48a95f37d7e2b7
zqumei <zaid.qumei+os@gmail.com>
2023-04-29 17:45:25 -0400

    Task 2: Added Setter and Getter for Account Class

--- src/pojo/TradeAccount.java
+++ src/pojo/TradeAccount.java
@@ -3,4 +3,12 @@ package pojo;
 public abstract class TradeAccount {
   private String id;
 
+  public String getId() {
+    return this.id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
 }

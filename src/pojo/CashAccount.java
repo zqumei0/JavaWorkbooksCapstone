0f7a57870eef35ab2ba3fee228e67ff7e22390bf
zqumei <zaid.qumei+os@gmail.com>
2023-04-29 17:49:18 -0400

    Task 6: Create setter and getter for Cash Account class

--- src/pojo/CashAccount.java
+++ src/pojo/CashAccount.java
@@ -5,4 +5,12 @@ import java.math.BigDecimal;
 public class CashAccount extends TradeAccount {
   private BigDecimal cashBalance;
 
+  public BigDecimal getCashBalance() {
+    return this.cashBalance;
+  }
+
+  public void setCashBalance(BigDecimal cashBalance) {
+    this.cashBalance = cashBalance;
+  }
+
 }

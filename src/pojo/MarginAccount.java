bc563e851e879b56010bed8044731a0813451d05
zqumei <zaid.qumei+os@gmail.com>
2023-04-30 00:05:01 -0400

    Task 11: Created constructor for MarginAccount

--- src/pojo/MarginAccount.java
+++ src/pojo/MarginAccount.java
@@ -2,8 +2,13 @@ package pojo;
 
 import java.math.BigDecimal;
 
-public class MarginAccount  {
+public class MarginAccount  extends TradeAccount{
   private BigDecimal margin;
+
+  public MarginAccount(String id, BigDecimal margin) {
+    super(id);
+    this.margin = margin;
+  }
 
   public BigDecimal getMargin() {
     return this.margin;

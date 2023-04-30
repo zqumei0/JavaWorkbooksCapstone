7deba87fd23c3a9c11b922d78e32601df4e11b8f
zqumei <zaid.qumei+os@gmail.com>
2023-04-30 00:00:47 -0400

    Task 10: Created Setters and Gettings for Margin Account

--- src/pojo/MarginAccount.java
+++ src/pojo/MarginAccount.java
@@ -5,4 +5,12 @@ import java.math.BigDecimal;
 public class MarginAccount  {
   private BigDecimal margin;
 
+  public BigDecimal getMargin() {
+    return this.margin;
+  }
+
+  public void setMargin(BigDecimal margin) {
+    this.margin = margin;
+  }
+
 }
